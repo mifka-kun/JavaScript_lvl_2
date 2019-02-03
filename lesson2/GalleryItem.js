f21486b400e3ecde7bdb2b3abe77c5832e5615a6
mifka-kun <39486859+mifka-kun@users.noreply.github.com>
2019-02-04 01:40:18 +0300

    добавлены файлы домашнего задания

---
+++ lesson2/GalleryItem.js
@@ -0,0 +1,9 @@
+class GalleryItem{
+    constructor(href, alt){
+        this.href = href;
+        this.alt  = alt;
+    }
+    render(){
+        return `<a href="${this.href}"><img src=${this.href} alt=${this.alt}</a></li>`
+    }
+}
